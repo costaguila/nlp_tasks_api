612562228b1e4bd849e6318da86251327dff7c0d
Guilherme de Almeida Costa <costaguila@gmail.com>
2020-09-22 18:35:23 -0300

    Adicionando view para o processamento dos dados.

--- nlp_tasks_api/apps/text_processing/views.py
+++ nlp_tasks_api/apps/text_processing/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import authentication, permissions
+from django.contrib.auth.models import User
 
-# Create your views here.
+
+
+class ListUsers(APIView):
+    authentication_classes = []
+    permission_classes = []
+
+    def post(self, request, format=None):
+        """
+            Recebe uma requizição com n textos.
+            Extrai o vocabulario, e a frequência das palavras
+        """
+        pass
